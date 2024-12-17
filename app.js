5ff9f2f8295053137747aafbfd32fa9d59a3dc6c
Monika <mciskowska@gmail.com>
2024-12-17 19:05:48 +0000

    app.js created file

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
+
 const app = express();
 const PORT = 3000;
+
 
 import {
   getQuotes,
@@ -10,11 +12,16 @@ import {
   deleteQuote,
 } from "./quote.js";
 
+const quotes = getQuotes();
+
 app.use(express.json());
 
 app.get("/", function (req, res) {
   res.send("Welcome to the inspirational quotes API");
 });
+  app.get("/quotes", function (req, res) {
+    res.send(getQuotes());
+  });
 
 app.listen(PORT, function () {
   console.log(`Server is now listening on http://localhost:${PORT}`);
